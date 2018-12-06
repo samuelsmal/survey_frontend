c84723e83d122e73370cf98e777a873e3fb9f3c5
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2018-12-06 11:22:30 +0100

    THIS COMMIT HAS NO PARENTS

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Survey Frontend
+
+Shitty but working frontend for the survey. With timer! And React!
+
+Nico would be proud.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TimerMixin from 'react-timer-mixin';
+
+// TODO send responses to server
+// TODO auth with server (session cookie should be enough)
+// TODO some styling
+// TODO maybe show a countdown?
+// TODO make question change more visible
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '',
+      answers: [],
+      questions: ['brick', 'toothbrush', 'gummy beer'],
+      current_question_id: 0,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = TimerMixin.setInterval(() => {
+      console.log("next question");
+      if (this.state.current_question_id < this.state.questions.length - 1) {
+        this.setState({current_question_id: this.state.current_question_id + 1})
+      }
+    }, 6000);
+  }
+
+  compnentWillUnmount() {
+    TimerMixin.clearInterval(this.interval);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.setState({answers: this.state.answers.concat([this.state.current_question_id, 
+      this.state.value, Date.now()]), value: ''},
+      () => {
+        console.log(this.state.answers);
+      }
+    );
+
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Press <b>ENTER</b> after each new word</p>
+        <form onSubmit={this.handleSubmit} className="form">
+          <label className="label">Come up with as many uses for object <b>{this.state.questions[this.state.current_question_id]}</b></label>
+          <input className="textarea" type="textarea" value={this.state.value} onChange={this.handleChange}/>
+          <input className="submit_btn" type="submit" value="Submit"/>
+        </form>
+
       </div>
     );
   }
